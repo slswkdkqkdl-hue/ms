871edb40d61b86d5e474b9be3ff1b5b47d204839
slswkdkqkdl-hue <slswkdkqkdl@gmail.com>
2026-01-05 10:20:48 +0900

    예

--- c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccab294621c206d033bff1b3ce2a1bb1d2ee71d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
