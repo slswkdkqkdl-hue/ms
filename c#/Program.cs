858d033fe8e30bbbefef9c40353b187a3cbbc902
slswkdkqkdl-hue <slswkdkqkdl@gmail.com>
2026-01-06 09:15:13 +0900

    5

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Text;
 
 class Program
 {
     static void Main()
-    {   
-        const int max = 4;
-        const int gold = 1000;
-        const string version = "1.0.0";
-
-        Console.WriteLine("=== 게임 설정 ===");
-        Console.WriteLine($"최대 플레이어 : {max}명");
-        Console.WriteLine($"시작 골드 {gold}G");
-        Console.WriteLine($"버전{version}");
-
+    {
         
+       float speed = 5.5f;
+       double attack_speed = 1.25;
+       decimal item_gold = 12.99m;
+
+
+       Console.WriteLine("=== 캐릭터 능력치 ===");
+       Console.WriteLine("이동 속도 : "+speed);
+       Console.WriteLine("공격 속도 : "+attack_speed);
+       Console.WriteLine("아이템 가격 : "+item_gold);
+       
+
+
+
     }
 }
-    
--- c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2a89770547a72929d4de7333d010c1299fa91d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e07ae03870b55de17c2c40afeb639eb59b0cda")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
